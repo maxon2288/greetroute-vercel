6948559121e06322695b3e2394f222c49b94c73f
maxon2288 <maxswim228@gmail.com>
2023-12-06 14:38:22 +0100

    docker update 1111444

--- src/app/components/ui/footer.jsx
+++ src/app/components/ui/footer.jsx
@@ -1,28 +1,25 @@
-'use client'
+"use client"
 
-import Link from 'next/link'
+import Link from "next/link"
 
 export default function Footer() {
 	const year = new Date().getFullYear()
 	return (
 		<>
-			<div className='footer'>
-				<div className='wrapper'>
+			<div className="footer">
+				<div className="wrapper">
 					<div>
-						<Link href='/src/pages'>
-							<img src='/img/svg/footer-logo.svg' alt='' />
+						<Link href="/src/pages">
+							<img src="/img/svg/footer-logo.svg" alt="" />
 						</Link>
 						© GreetRoute {year}. Все права защищены
 					</div>
 					<ul>
 						<li>
-							<Link href='/privacy-policy'>Политика конфиденциальности</Link>
+							<Link href="/privacy-policy">Политика конфиденциальности</Link>
 						</li>
 						<li>
-							<Link href='/public-offer'>Публичная оферта</Link>
-						</li>
-						<li>
-							<Link href='/consent'>Обработка данных</Link>
+							<Link href="/consent">Обработка данных</Link>
 						</li>
 					</ul>
 				</div>
