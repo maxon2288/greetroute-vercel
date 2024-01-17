fb363188eae41db099b2d5d0742ece4d4a76fc5b
maxon2288 <maxswim228@gmail.com>
2024-01-17 15:40:22 +0100

    docker update 1111444

--- src/app/page.js
+++ src/app/page.js
@@ -46,7 +46,7 @@ export default function Home() {
 			<meta name="description"
 				  content="GreetRoute - агентство по разработке и продвижению сайтов в Казахстане. Оставьте заявку и мы вас проконсультируем!" />
 			<title>Помогаем бизнесу найти клиентов | GreetRoute</title>
-			{/*<meta name="yandex-verification" content="9522d3c093cb1db7" />*/}
+			<meta name="yandex-verification" content="0268f3ced9e26acd" />
 
 			<Header />
 			<div className="first">
